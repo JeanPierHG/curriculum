7390d8210104cfb645e8f8912a0031455b681b4d
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-25 14:14:29 -0500

    tech icons and competencias

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { SiGithub, SiLinkedin } from 'react-icons/si'
 import { IoBagSharp } from 'react-icons/io5'
+import {
+  SiDocker,
+  SiExpress,
+  SiFramer,
+  SiGit,
+  SiGithub,
+  SiLinkedin,
+  SiLinux,
+  SiMaterialui,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+  SiVisualstudiocode,
+} from 'react-icons/si'
 import imageFace from '../assets/profile.png'
 import styles from '../styles/Home.module.css'
 
@@ -19,11 +36,12 @@ const Home: NextPage = () => {
       <main className={styles.container}>
         <div className={styles.container__myprofile}>
           <Image
-            width={400}
-            height={400}
+            width={300}
+            height={300}
             className={styles.myface}
             src={imageFace}
             alt={'my-face'}
+            objectFit='cover'
           />
           <div className={styles.container__info}>
             <div>
@@ -64,43 +82,105 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className={styles.container__data}>
-          <div className={styles.container__skills}>
-            <h2>Tech Skills</h2>
-            <div className={styles.container__tech}>
-              <div>
-                <h3>Front-end</h3>
-                <ul>
-                  <li>React</li>
-                  <li>Redux</li>
-                  <li>Next.js</li>
-                  <li>TypeScript</li>
-                  <li>Framer-Motion</li>
-                  <li>MaterialUI</li>
-                  <li>NextUI</li>
-                </ul>
+          <div className={styles.container__leftSide}>
+            <div className={styles.container__skills}>
+              <h2 className={styles.title__skils}>Tech Skills</h2>
+              <div className={styles.container__tech}>
+                <div>
+                  <h3>Front-end</h3>
+                  <ul>
+                    <li>
+                      <SiReact /> React
+                    </li>
+                    <li>
+                      <SiRedux /> Redux
+                    </li>
+                    <li>
+                      {' '}
+                      <SiNextdotjs /> Next.js
+                    </li>
+                    <li>
+                      {' '}
+                      <SiTypescript /> TypeScript
+                    </li>
+                    <li>
+                      {' '}
+                      <SiFramer /> Framer-Motion
+                    </li>
+                    <li>
+                      <SiMaterialui /> MaterialUI
+                    </li>
+                    <li>
+                      {' '}
+                      <SiNextdotjs />
+                      NextUI
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h3>Back-end</h3>
+                  <ul>
+                    <li>
+                      {' '}
+                      <SiNodedotjs /> Node.js
+                    </li>
+                    <li>
+                      <SiExpress /> Express
+                    </li>
+                    <li>
+                      <SiPostgresql /> PostgreSQL
+                    </li>
+                    <li>
+                      <SiMongodb /> Mongodb
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h3>Tools</h3>
+                  <ul>
+                    <li>
+                      {' '}
+                      <SiVisualstudiocode /> Visual Studio Code
+                    </li>
+                    <li>
+                      <SiLinux /> Linux
+                    </li>
+                    <li>
+                      {' '}
+                      <SiDocker />
+                      Docker
+                    </li>
+                    <li>
+                      <SiGit /> Git
+                    </li>
+                    <li>
+                      <SiGithub /> Git-Hub
+                    </li>
+                  </ul>
+                </div>
               </div>
+            </div>
+            <div className={styles.container__competencias}>
+              <h2>Competencias</h2>
               <div>
-                <h3>Back-end</h3>
                 <ul>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                  <li>PostgreSQL</li>
-                  <li>Mongodb</li>
-                </ul>
-              </div>
-              <div>
-                <h3>Tools</h3>
-                <ul>
-                  <li>Visual Studio Code</li>
-                  <li>Linux</li>
-                  <li>Docker</li>
-                  <li>Git & Github</li>
+                  <li>Trabajo en equipo</li>
+                  <li>Creatividad</li>
+                  <li>Colaboracion</li>
+                  <li>Dedicación</li>
+                  <li>Inteligencia Emocional</li>
+                  <li>Proactividad</li>
                 </ul>
               </div>
             </div>
+            <div className={styles.container__hobbies}>
+              <h2>Hobbies</h2>
+            </div>
           </div>
           <div className={styles.container__history}>
-            <h2>Experienca Laboral</h2>
+            <div>
+              <h2>Experienca Laboral</h2>
+            </div>
           </div>
         </div>
       </main>
